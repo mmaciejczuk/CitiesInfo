3578f79492193d6e9e176fa62481ffa4cca6ad15
Mariusz Maciejczuk <mariusz.maciejczuk@corp.smtsoftware.com>
2017-05-31 10:58:42 +0200

    changes

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -12,7 +12,8 @@ namespace CityInfo.API.Controllers
         {
             return new JsonResult(new List<object>()
             {
-
+                new { id = 1, Name = "New York City" },
+                new { id = 2, Name = "Antwerp" }
             });
         }
     }
